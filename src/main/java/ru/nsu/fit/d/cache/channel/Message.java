343fc9ddfd1243efd4a029f0b0e75359eb149cdc
Sergey <ogresed@gmail.com>
2020-01-19 19:49:46 +0700

    переписан ресивер

--- src/main/java/ru/nsu/fit/d/cache/channel/Message.java
+++ src/main/java/ru/nsu/fit/d/cache/channel/Message.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Message<T> {
+public class Message<T extends Serializable> {
 	
 	private MessageType messageType;
 	
--- src/main/java/ru/nsu/fit/d/cache/channel/Receiver.java
+++ src/main/java/ru/nsu/fit/d/cache/channel/Receiver.java
@@ -1,81 +1,96 @@
 package ru.nsu.fit.d.cache.channel;
 
+import com.google.gson.Gson;
 import lombok.AllArgsConstructor;
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
 import ru.nsu.fit.d.cache.event.Event;
 import ru.nsu.fit.d.cache.event.EventQueue;
+import ru.nsu.fit.d.cache.event.EventType;
 
-import java.io.IOException;
-import java.net.*;
-import java.rmi.UnknownHostException;
+import java.io.Serializable;
 import java.util.Arrays;
 
 @AllArgsConstructor
-public class Receiver implements Runnable {
-	
+public class Receiver<T extends Serializable> implements Runnable {
+	private final Gson gson = new Gson();
 	private static final int WAIT_TIMEOUT = 200;
-
 	private String endpoint;
-
-	private EventQueue eventQueue;
-	
+	private EventQueue<T> eventQueue;
 	private int port;
+	private final ZContext context = new ZContext();
+	private final ZMQ.Socket multicastSocket = context.createSocket(SocketType.SUB);
+	private final ZMQ.Socket forNewbieSocket = context.createSocket(SocketType.PAIR);
 
-	InetAddress multicastGroup;
-	MulticastSocket receiveMulticsSocket;
-	byte[] multicastBuf;
-	DatagramPacket multicastReceivePacket;
-
-	public Receiver(int port, String endpoint, EventQueue eventQueue) {
+	public Receiver(int port, String endpoint, EventQueue<T> eventQueue) {
 		this.port = port;
 		this.endpoint = endpoint;
 		this.eventQueue = eventQueue;
-
-		try {
-			multicastGroup = InetAddress.getByName(endpoint);
-			receiveMulticsSocket = new MulticastSocket(port);
-			receiveMulticsSocket.joinGroup(multicastGroup);
-			multicastBuf= new byte[1024];
-			multicastReceivePacket = new DatagramPacket(multicastBuf, multicastBuf.length);
-		} catch (UnknownHostException e) {
-			System.out.println("Unknown Host");
-		} catch (IOException e) {
-			System.out.println("impossible create socket");
-		}
+		multicastSocket.connect(endpoint + ":" + port);
 	}
 
-	/**
-	 *получение сообщений с мультикаста
-	 * */
-	public Message receive() {
-		
+	public Message<T> receive() {
 		// TODO: 18.01.20 Запускается в отдельном потоке, блокируется и ждет запросы.
 		//  Кладет полученное в очередь как event. Далее из этой очереди класс Node будет выгребать запросы и реагировать
+		multicastSocket.setReceiveTimeOut(WAIT_TIMEOUT);
+		//reading
+		byte[] buf = new byte[4096];
+		StringBuilder messageBuilder = new StringBuilder();
+		while(multicastSocket.recv(buf, 0, buf.length, 0) > 0) {
+			messageBuilder.append(Arrays.toString(buf));
+		}
+		String messageString = messageBuilder.toString();
+		Message<T> message = gson.fromJson(messageString, Message<T>.class);
+		//TODO: 19.01.20 решить, что дедать с параметризованым классом Message
+		return message;
+	}
 
-		try {
-			receiveMulticsSocket.setSoTimeout(WAIT_TIMEOUT);
-		} catch (SocketException e) {
-			e.printStackTrace();
+	public Message<T> receiveFromNewbie() {
+		forNewbieSocket.setReceiveTimeOut(WAIT_TIMEOUT);
+		//reading
+		byte[] buf = new byte[4096];
+		StringBuilder messageBuilder = new StringBuilder();
+		while(multicastSocket.recv(buf, 0, buf.length, 0) > 0) {
+			messageBuilder.append(Arrays.toString(buf));
 		}
-		try {
-			receiveMulticsSocket.receive(multicastReceivePacket);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		String messageString = Arrays.toString(multicastReceivePacket.getData());
-		Message message = null;
-		//TODO: 19.01.20 десириализовать messageString -> message
+		String messageString = messageBuilder.toString();
+		Message<T> message = (Message<T>)gson.fromJson(messageString, Message.class);
+		//TODO: 19.01.20 решить, что дедать с параметризованым классом Message
 		return message;
 	}
 
 	public void run() {
-		// мультикаст сокет
-		while (true) {
-			Message message = receive();
+		while (!Thread.currentThread().isInterrupted()) {
+			Message<T> message = receive();
 			//TODO: 19.01.20 положить новый эвент согласно полученому сообщению
-			Event event = null;
-			eventQueue.add(event);
+			Event<T> event = createEventByMessage(message);
+			if(event != null)
+				eventQueue.add(event);
+
+			//получение сообщения от новичка
+			Message<T> newbieMessage = receive();
+			//TODO: 19.01.20 положить новый эвент согласно полученому сообщению
+			Event<T> newbieEvent = createEventByMessage(message);
+			if(event != null)
+				eventQueue.add(event);
 		}
 	}
 
+	private Event<T> createEventByMessage(Message<T> message) {
+		if(message == null)
+			return null;
+		Event<T> event = new Event<T>();
+		event.setValue(message.getValue());
+		event.setResponseContext(message);
+		event.setChangeId(message.getChangeId());
+		event.setFromUrl(endpoint);
+		event.setKey(message.getKey());
+		//event.setLowPriorityValue();
+        if(message.getMessageType() == MessageType.CONFIRMATION) {
+            event.setEventType(EventType.CONFIRMATION);
 
+        }
+        return  null;
+	}
 }
